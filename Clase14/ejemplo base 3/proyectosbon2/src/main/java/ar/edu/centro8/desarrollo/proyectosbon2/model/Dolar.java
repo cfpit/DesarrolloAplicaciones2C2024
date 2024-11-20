e62ac12225e70e4ca6a0c75a5a4a0ae5c1d680a8
cfpit <127329179+cfpit@users.noreply.github.com>
2024-11-20 10:36:58 -0300

    Add files via upload

---
+++ Clase14/ejemplo base 3/proyectosbon2/src/main/java/ar/edu/centro8/desarrollo/proyectosbon2/model/Dolar.java
@@ -0,0 +1,29 @@
+/*El anotador @Data de Lombok genera los siguientes métodos automáticamente para la clase
+ anotada:
+
+Constructor vacío (default())
+Constructor con todos los campos (allArgsConstructor())
+Método getter para cada campo (getter())
+Método setter para cada campo (setter())
+Método toString() que incluye todos los campos de la clase
+Método equals() y hashCode() basados en todos los campos de la clase*/
+
+package ar.edu.centro8.desarrollo.proyectosbon2.model;
+
+
+// Toma el tiempo UTC (Coordinated Universal Time).
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class Dolar {
+    
+    private String moneda;
+    private String casa;
+    private String nombre;
+    private Double compra;
+    private Double venta;
+    private Instant fechaActualizacion;
+
+}
